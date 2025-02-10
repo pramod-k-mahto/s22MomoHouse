d99be6cee06d1acddff92f1d8307b6f591da84a8
Pramod Mahto <Mahtop324@gmail.com>
2025-02-10 12:25:12 +0545

    added image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 function Home() {
-  return <div className="bg-amber-500">Home</div>;
+  return <div className="bg-amber-500">Home
+  
+  
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFUAfyVe3Easiycyh3isP9wDQTYuSmGPsPQvLIJdEYvQ_DsFq5Ez2Nh_QjiS3oZ3B8ZPfK9cZQyIStmQMV1lDPLw" alt="" /></div>;
 }
 
 export default Home;
